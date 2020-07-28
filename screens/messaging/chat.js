ed924f2a87ecf0edc6a011a43088b96f95119c71
Shifa Somji <shifamsomji@gmail.com>
2020-07-28 14:06:57 -0700

    starting the messaging tutorial

---
+++ screens/messaging/chat.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import ToggleSwitch from "../../components/toggleSwitch";
+
+export default function Navigation() {
+    return (
+        <ToggleSwitch />
+    );
+  }
