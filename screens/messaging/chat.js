1ab0657297bae26f7b6aece537835b72d9a37ed0
Shifa Somji <shifamsomji@gmail.com>
2020-07-29 14:41:05 -0700

    more gifted chat things

--- screens/messaging/chat.js
+++ screens/messaging/chat.js
@@ -15,23 +15,61 @@ export default class Example extends Component {
     messages: [],
   }
 
+
   componentWillMount() {
     this.setState({
       messages: [
         {
-          _id: 1,
-          text: 'Hello developer',
+          _id: Math.round(Math.random() * 1000000),
+          text: 'Yes, and I use Gifted Chat!',
+          createdAt: new Date(),
+          user: {
+            _id: 1,
+            name: 'Shifa Somji',
+          },
+        },
+        {
+          _id: Math.round(Math.random() * 1000000),
+          text: 'Are you building a chat app?',
           createdAt: new Date(),
           user: {
             _id: 2,
             name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
           },
         },
+        {
+          _id: 1,
+          text: 'This is a quick reply. Do you love Gifted Chat?',
+          createdAt: new Date(),
+          quickReplies: {
+            type: 'radio', // or 'checkbox',
+            keepIt: true,
+            values: [
+              {
+                title: '😋 Yes',
+                value: 'yes',
+              },
+              {
+                title: '📷 Yes, let me show you with a picture!',
+                value: 'yes_picture',
+              },
+              {
+                title: '😞 Nope. What?',
+                value: 'no',
+              },
+            ],
+          },
+          user: {
+            _id: 2,
+            name: 'React Native',
+          },
+        },
+        
       ],
     })
   }
 
+  // Adds the new message to the array of messages in the state
   onSend(messages = []) {
     this.setState(previousState => ({
       messages: GiftedChat.append(previousState.messages, messages),
@@ -44,7 +82,7 @@ export default class Example extends Component {
         messages={this.state.messages}
         onSend={messages => this.onSend(messages)}
         user={{
-          _id: 1,
+          _id: 1, // TO DO: figure out what this does, i'm not sure yet
         }}
       />
     )
