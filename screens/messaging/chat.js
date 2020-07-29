e41aa45d2772b8fdebc5aff81b7eaf08bed88a9e
Shifa Somji <shifamsomji@gmail.com>
2020-07-29 13:23:11 -0700

    chat screen is made and you can sort of message with react native's gifted chat

--- screens/messaging/chat.js
+++ screens/messaging/chat.js
@@ -1,8 +1,52 @@
 import React, { Component } from 'react';
-import ToggleSwitch from "../../components/toggleSwitch";
+/*import ToggleSwitch from "../../components/toggleSwitch";
 
 export default function Navigation() {
     return (
         <ToggleSwitch />
     );
   }
+*/
+
+import { GiftedChat } from 'react-native-gifted-chat'
+
+export default class Example extends Component {
+  state = {
+    messages: [],
+  }
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  render() {
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+    )
+  }
+}
