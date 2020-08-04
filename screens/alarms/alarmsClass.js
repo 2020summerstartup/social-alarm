6c62702799a60eb6a7777a2ec2485ab491fc154d
Shifa Somji <shifamsomji@gmail.com>
2020-08-04 09:58:51 -0700

    removed a bug in the imports

--- screens/alarms/alarmsClass.js
+++ screens/alarms/alarmsClass.js
@@ -16,8 +16,6 @@ import { appStyles } from '../../style/stylesheet';
 import DatePicker from 'react-native-datepicker';
 import RNPickerSelect from 'react-native-picker-select';
 
-import getStyleSheet from '../../style/theme';
-
 import * as firebase from "firebase";
 import { db, auth } from "../../firebase/firebase";
 
