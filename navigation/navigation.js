388ef0053f032cb53fa9dc633456f4e6de04fc47
Anna <asinger@hmc.edu>
2020-08-10 16:34:26 -0700

    docs(navigation): commented navigation

--- navigation/navigation.js
+++ navigation/navigation.js
@@ -2,13 +2,11 @@ import React, { useEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import GroupScreen from "../screens/groups/groups";
-import AlarmScreen from '../screens/alarms/alarmsClass';
+import AlarmScreen from "../screens/alarms/alarmsClass";
 import ProfileScreen from "../screens/profile/profile";
 import { NavigationContainer } from "@react-navigation/native";
-import {APPBACKGROUNDCOLOR, APPTEXTRED, APPTEXTWHITE} from '../style/constants';
-import { NotificationContext }  from "../contexts/NotificationContext";
-import {db, auth} from "../firebase/firebase";
-
+import { NotificationContext } from "../contexts/NotificationContext";
+import { db, auth } from "../firebase/firebase";
 
 /* navigation.js
  * bottom tab navigator for signed in user
@@ -19,7 +17,6 @@ import {db, auth} from "../firebase/firebase";
 // This is code to create a bottom tab navigator
 // Code from https://reactnavigation.org/docs/bottom-tab-navigator/
 export default function Navigation() {
-
   return (
     <NavigationContainer>
       <MyTabs />
@@ -27,128 +24,122 @@ export default function Navigation() {
   );
 }
 
-
 const Tab = createBottomTabNavigator();
-
 
 // Add more screens as necessary
 function MyTabs() {
-
-  useEffect(()=> {
-
-    initializeNotifications()
-
-  })
+  // like componentDidMount - called when app loads
+  useEffect(() => {
+    initializeNotifications();
+  });
 
   return (
+    <NotificationContext.Consumer>
+      {(context) => {
+        // notification  and theme stuff
+        const {
+          notificationCount,
+          setNotificationCount,
+          isDarkMode,
+          light,
+          dark,
+        } = context;
 
-    <NotificationContext.Consumer>{(context) => {
-      
-      const {notificationCount, setNotificationCount, isDarkMode, light, dark} = context
+        const theme = isDarkMode ? dark : light;
 
-      const theme = isDarkMode ? dark : light
+        // called in useEffect
+        // sets the notificationCount (context) to correct number via firebase
+        initializeNotifications = () => {
+          db.collection("users")
+            .doc(auth.currentUser.email)
+            .get()
+            .then((doc) => {
+              setNotificationCount(doc.data().alertQueue.length.toString());
+            });
+        };
 
-      initializeNotifications = () => {
-        db.collection("users").doc(auth.currentUser.email).get().then((doc) => {
-          setNotificationCount(doc.data().alertQueue.length.toString())
-        })
-      }
+        return (
+          <Tab.Navigator
+            initialRoutename="Alarms" // After user signs in, go to alarms page
+            tabBarOptions={{
+              activeTintColor: theme.APPTEXTRED, // This makes the button pink when you're on that page
+              activeBackgroundColor: theme.APPBACKGROUNDCOLOR,
+              inactiveBackgroundColor: theme.APPBACKGROUNDCOLOR,
+            }}
+          >
+            <Tab.Screen
+              name="Alarms"
+              component={AlarmScreen}
+              options={{
+                tabBarLabel: "Alarms",
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialCommunityIcons
+                    name="alarm-multiple"
+                    color={color}
+                    size={size}
+                  />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Groups"
+              component={GroupScreen}
+              options={{
+                tabBarLabel: "Groups",
+                tabBarIcon: ({ color, size }) => (
+                  <MaterialCommunityIcons
+                    name="account-group"
+                    color={color}
+                    size={size}
+                  /> // Default color and size: white and 20
+                ),
+                //tabBarVisible: false,
+              }}
+            />
 
+            {/* profile screen (there are two things here - 
+                only one will be displayed depending on if there are notifications ) */}
 
-      return(
+            {/* if there are notifications have a badge with notificationCount */}
+            {notificationCount > 0 && (
+              <Tab.Screen
+                name="Profile"
+                component={ProfileScreen}
+                options={{
+                  tabBarLabel: "Profile",
+                  // this controls the badge
+                  tabBarBadge: notificationCount,
+                  tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons
+                      name="account"
+                      color={color}
+                      size={size}
+                    />
+                  ),
+                }}
+              />
+            )}
 
-
-    <Tab.Navigator
-    initialRoutename="Alarms" // After user signs in, go to alarms page
-    tabBarOptions={{
-      activeTintColor: theme.APPTEXTRED, // This makes the button pink when you're on that page
-      activeBackgroundColor: theme.APPBACKGROUNDCOLOR,
-      inactiveBackgroundColor: theme.APPBACKGROUNDCOLOR,
-    }}
-  >
-    <Tab.Screen
-      name="Alarms"
-      component={AlarmScreen}
-      options={{
-        tabBarLabel: "Alarms",
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons
-            name="alarm-multiple"
-            color={color}
-            size={size}
-          />
-        ),
+            {/* if there aren't notifications, just show normal button (no badge) */}
+            {notificationCount <= 0 && (
+              <Tab.Screen
+                name="Profile"
+                component={ProfileScreen}
+                options={{
+                  tabBarLabel: "Profile",
+                  tabBarIcon: ({ color, size }) => (
+                    <MaterialCommunityIcons
+                      name="account"
+                      color={color}
+                      size={size}
+                    />
+                  ),
+                }}
+              />
+            )}
+          </Tab.Navigator>
+        );
       }}
-
-    />
-    <Tab.Screen
-      name="Groups"
-      component={GroupScreen}
-      options={{
-        tabBarLabel: "Groups",
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="account-group" color={color} size={size} /> // Default color and size: white and 20
-        ),
-        //tabBarVisible: false,
-      }}
-    />
-{/*
-    <Tab.Screen
-      name="Stopwatch"
-      component={StopwatchScreen}
-      options={{
-        tabBarLabel: "Stopwatch",
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons
-            name="clock-fast"
-            color={color}
-            size={size}
-          />
-        ),
-      }}
-    />
-    */}
-
-    {notificationCount > 0 && (
-      <Tab.Screen
-      name="Profile"
-      component={ProfileScreen}
-      options={{
-        tabBarLabel: "Profile",
-        
-        tabBarBadge: notificationCount,
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="account" color={color} size={size} />
-        ),
-      }}
-    />
-    )}
-
-    {notificationCount <= 0 && (
-
-      <Tab.Screen
-      name="Profile"
-      component={ProfileScreen}
-      options={{
-        tabBarLabel: "Profile",
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="account" color={color} size={size} />
-        ),
-      }}
-    />
-    )}
-
-    
-    
-  </Tab.Navigator>
-
-
-
-      )
-    }}
-    
     </NotificationContext.Consumer>
-
-
   );
 }
