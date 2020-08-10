da601cc01645388b83c3cb2911d8f9abc4c11941
Shifa Somji <shifamsomji@gmail.com>
2020-08-10 13:45:24 -0700

    refactor(profile): removed the unused profile page code, made time zones better

--- screens/profile/profile.js
+++ screens/profile/profile.js
@@ -1,28 +1,286 @@
-import React from "react";
-import {
-  StyleSheet,
-  View,
-} from "react-native";
-import Profile3 from './Profile3';
+import React, { Component, useState } from 'react'
+import { ScrollView, Switch, StyleSheet, Dimensions, Text, View, Linking, AsyncStorage, TouchableOpacity, DevSettings, Button } from 'react-native'
+import { Avatar, ListItem } from 'react-native-elements'
+import { auth } from "../../firebase/firebase";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import RNPickerSelect from 'react-native-picker-select';
+import { profileStyles } from '../../style/stylesheet';
 
-/* profile.js
- * Profile screen
- * TO DO: delete this file? all the code is in Profile3.js
+import {APPBACKGROUNDCOLOR, APPTEXTRED, APPTEXTBLUE, APPTEXTWHITE} from '../../style/constants';
+
+// chevron is the greater than sign that's on the right of everything on the profile page
+import Chevron from './Chevron'
+
+// sets the styles for all the icons
+import BaseIcon from './Icon'
+
+/* profile3.js
+ * profile screen 
+ * has push notifications, birthday, time zone, about us,  
+ * share our app, and send feedback
+ * feel free to change or delete any of these 
  */
 
-export default function Profile() {
+// This is for the birthday picker
+const BirthdayPicker = () => {
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [show, setShow] = useState(false);
+  const [date, setDate] = useState(new Date());
+
+  // shows the date picker
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios');
+    setDate(currentDate);
+  };
+
+  // TO DO: replace the "select birthday" button title with the date
+  const handleConfirm = () => {
+    hideDatePicker();
+  };
+
   return (
-    <View style={styles.container}>
-      <Profile3/>
+    <View>
+      <Button title="Select birthday" onPress={showDatePicker}/>
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        minimumDate={new Date(1950, 0, 1)}
+        maximumDate={new Date(2020, 11, 31)}
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
+        style={profileStyles.birthdayBtn}
+        headerTextIOS={"When's your birthday?"}
+        onChange={onChange}
+      />
     </View>
   );
+};
+
+class ProfileScreen extends Component {
+
+  // sign out functionality 
+  signOutUser = async () => {
+     await AsyncStorage.removeItem("email");
+     await AsyncStorage.removeItem("password");
+     //navigation.navigate("Auth");
+     auth
+       .signOut()
+       .then(() => {
+         // this reloads the page
+         DevSettings.reload();
+       })
+       .catch((error) => console.log(error));
+   };
+  
+  componentDidMount() {
+    this.getEmailName();
+  }
+
+  // gets user email and name from async storage
+  getEmailName = async () => {
+    const userEmail = await AsyncStorage.getItem("email"); 
+    const userName = await AsyncStorage.getItem("name");
+    this.setState({name: userName});
+    this.setState({email: userEmail});
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      switchValue: false,
+      name: "", // this is the user's name
+      email: "", // this is the user's email
+      timeZoneSelected: "",
+      timezoneArray: [
+        { label: 'Eastern Time', value: 'EST' },
+        { label: 'Central Time', value: 'CST' },
+        { label: 'Mountain Time', value: 'MST' },
+        { label: 'Pacific Time', value: 'PST' },
+      ]
+    };
+  }
+
+  toggleSwitch = value => {
+    this.setState({ switchValue: value });
+  };
+
+  render() {
+
+    return (
+      <ScrollView style={profileStyles.scroll}>  
+        {/* this part shows the user's name and email */}
+        <View style={profileStyles.userRow}>
+          <View>
+            <Text style={{ fontSize: 30, color: APPTEXTBLUE }}>{this.state.name}</Text>
+            <Text
+              style={{
+                color: APPTEXTBLUE,
+                fontSize: 25,
+              }}
+            >
+              {this.state.email} 
+            </Text>
+          </View>
+        </View>
+
+        <View>
+          <ListItem
+            hideChevron
+            title="Dark Mode"
+            containerStyle={profileStyles.listItemContainer}
+            rightElement={
+              <Switch
+                onValueChange={this.toggleSwitch}
+                value={this.state.switchValue}
+              />
+            }
+            leftIcon={
+              <BaseIcon
+                containerStyle={{
+                  backgroundColor: '#FFADF2',
+                }}
+                icon={{
+                  type: 'ionicon',
+                  name: 'ios-moon',
+                }}
+              />
+            }
+          />
+          <ListItem
+            title="Birthday"
+            rightTitleStyle={{ fontSize: 18 }}
+            rightElement={<BirthdayPicker/>}    
+            containerStyle={profileStyles.listItemContainer}
+            leftIcon={
+              <BaseIcon
+                containerStyle={{ backgroundColor: '#FAD291' }}
+                icon={{
+                  type: 'material',
+                  name: 'cake',
+                }}
+              />
+            }
+          />
+          <ListItem
+            title="Time Zone"
+            rightElement={
+              <RNPickerSelect
+                onValueChange={(value) => this.setState({ timeZoneSelected: value })}
+                items={this.state.timezoneArray}
+
+                // Object to overide the default text placeholder for the PickerSelect
+                placeholder={{label: 'Select timezone', value: 'select timezone'}}
+                style={
+                  { fontWeight: 'normal',
+                    color: 'black',
+                    placeholder: {
+                      color: "black",
+                      fontSize: 18,
+                      alignSelf: 'center',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      marginRight: 12,
+                      marginTop: 8,
+                    },
+                    inputIOS: {
+                      color: "black",
+                      fontSize: 16,
+                      marginRight: 12,
+                      marginTop: 8,
+                      alignSelf: 'center',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }
+                  }
+                }
+                doneText={"Select"}                 
+              />
+            }
+            rightTitleStyle={{ fontSize: 15 }}
+            containerStyle={profileStyles.listItemContainer}
+            leftIcon={
+              <BaseIcon
+                containerStyle={{ backgroundColor: '#57DCE7' }}
+                icon={{
+                  type: 'font-awesome',
+                  name: 'globe',
+                }}
+              />
+            }
+          />
+        </View>
+        <View>
+          <ListItem
+            title="About Us"
+            containerStyle={profileStyles.listItemContainer}
+            onPress={ ()=>{ Linking.openURL('https://www.github.com/2020summerstartup/social-alarm')}} // we can change this later 
+            leftIcon={
+              <BaseIcon
+                containerStyle={{ backgroundColor: '#A4C8F0' }}
+                icon={{
+                  type: 'ionicon',
+                  name: 'md-information-circle',
+                }}
+              />
+            }
+            rightIcon={<Chevron />}
+          />
+          <ListItem
+            title="Share our App"
+            onPress={ ()=>{ Linking.openURL('https://www.github.com/2020summerstartup/social-alarm')}}
+            containerStyle={profileStyles.listItemContainer}
+            leftIcon={
+              <BaseIcon
+                containerStyle={{
+                  backgroundColor: '#C47EFF',
+                }}
+                icon={{
+                  type: 'entypo',
+                  name: 'share',
+                }}
+              />
+            }
+            rightIcon={<Chevron />}
+          />
+          <ListItem
+            title="Send Feedback"
+            onPress={ ()=>{ Linking.openURL('https://www.github.com/2020summerstartup/social-alarm')}}
+            containerStyle={profileStyles.listItemContainer}
+            leftIcon={
+              <BaseIcon
+                containerStyle={{
+                  backgroundColor: '#00C001',
+                }}
+                icon={{
+                  type: 'materialicon',
+                  name: 'feedback',
+                }}
+              />
+            }
+            rightIcon={<Chevron />}
+          />
+        </View>
+        
+        {/* Sign out button */}
+        <TouchableOpacity
+          style={profileStyles.loginBtn}
+          onPress={() => this.signOutUser()}
+        >
+          <Text style={profileStyles.logo}>Sign Out</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fafafa",
-    alignItems: "center",
-    justifyContent: "flex-start",
-  },
-});
+export default ProfileScreen
